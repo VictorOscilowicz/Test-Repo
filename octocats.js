839614c71947252d3a8efc98d8212d9ae085ea62
VictorOscilowicz <7voscilowicz@stpatrick.org>
2016-10-06 11:51:58 -0500

    Add files via upload
    I began work on today's assignment.

---
+++ octocats.js
@@ -0,0 +1,17 @@
+//Write your constructor function below.
+//You should have 2 instance variables and 1 instance function included.
+var name=n;
+var arms=a;
+
+function Octocat(n,a){
+  this.octocatName=n
+  this.octocatArms=a
+
+  this.Slap = function(arms){
+    For(var arms=a,a<8,a++){
+      console.log("SLAP");
+  }
+}
+
+
+//Write your 3 new octocat objects below here.
